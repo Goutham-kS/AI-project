dd529f3b6a4a5f25220e1a970e51cbaccbbd143d
Goutham <105034862+Goutham-kS@users.noreply.github.com>
2025-10-06 14:59:34 +0000

    Add GitHub Actions workflow to build and publish MyApi

--- obj/Debug/net8.0/MyApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b26c2349eac95b4f2289c16050c62bad88c0f3d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14a58b76fe245c35f3446a77867eaa2e31fb207d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
